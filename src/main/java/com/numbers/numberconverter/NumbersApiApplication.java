b1eb700ea3cb69e55d9fb16c0e49cb5d2edb2710
Janakiraman <r.janakiraman06@gmail.com>
2021-09-14 08:36:41 -0600

    Initial commit - Project skeleton

---
+++ src/main/java/com/numbers/numberconverter/NumbersApiApplication.java
@@ -0,0 +1,13 @@
+package com.numbers.numberconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbersApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumbersApiApplication.class, args);
+    }
+
+}
