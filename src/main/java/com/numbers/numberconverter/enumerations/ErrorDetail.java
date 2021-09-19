28245ee8e2dde5f321be0ba9ef3eeec2f43e9678
Janakiraman <r.janakiraman06@gmail.com>
2021-09-19 12:21:36 -0600

    Changes for Exception handler to include generic exception

--- src/main/java/com/numbers/numberconverter/enumerations/ErrorDetail.java
+++ src/main/java/com/numbers/numberconverter/enumerations/ErrorDetail.java
@@ -3,7 +3,8 @@ package com.numbers.numberconverter.enumerations;
 public enum ErrorDetail {
     INVALID_INPUT ("INVALID_INPUT", "Input is invalid.  Please provide valid Integer within the range [1 - 3999]"),
     INVALID_INPUT_RANGE("INVALID_INPUT_RANGE", "Input is out of supported range.  Please provide valid number within the range [1 - 3999]"),
-    INPUT_EMPTY("INPUT_EMPTY", "Input is empty.  Please provide valid number within the range [1 - 3999]");
+    INPUT_EMPTY("INPUT_EMPTY", "Input is empty.  Please provide valid number within the range [1 - 3999]"),
+    INTERNAL_SERVER_ERROR("INTERNAL_SERVER_ERROR", "Something went wrong.  Please try again later");
     String errorCode;
     String errorMessage;
 
--- src/main/java/com/numbers/numberconverter/exception/handler/NumberConverterExceptionHandler.java
+++ src/main/java/com/numbers/numberconverter/exception/handler/NumberConverterExceptionHandler.java
@@ -12,30 +12,58 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Iterator;
 
 @ControllerAdvice
 public class NumberConverterExceptionHandler {
     Logger LOGGER = LoggerFactory.getLogger(NumberConverterExceptionHandler.class);
     @ExceptionHandler(value = NumberConverterException.class)
-    public final ResponseEntity<Object> handleNumberConverterException(NumberConverterException ex){
+    public final ResponseEntity<NumberConverterResponse> handleNumberConverterException(NumberConverterException ex, WebRequest webRequest){
+        ServletWebRequest request = (ServletWebRequest) webRequest;
+        LOGGER.error("Requested URI: "+request.getRequest().getRequestURI() +" - Input range is invalid. Required input range: [1,3999].  Provided input: "+getQueryParameter(webRequest));
         NumberConverterError error = new NumberConverterError(ex.getErrorDetail());
         NumberConverterResponse response = new NumberConverterResponse(error);
         LOGGER.error(response.toString());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(value = MissingServletRequestParameterException.class)
-    public final ResponseEntity<NumberConverterResponse> handleMissingServletRequestParameterException(){
+    public final ResponseEntity<NumberConverterResponse> handleMissingServletRequestParameterException(WebRequest webRequest){
+        ServletWebRequest request = (ServletWebRequest) webRequest;
+        LOGGER.error("Requested URI: "+request.getRequest().getRequestURI() +" - Input is empty");
         NumberConverterError error = new NumberConverterError(ErrorDetail.INPUT_EMPTY);
         NumberConverterResponse response = new NumberConverterResponse(error);
         LOGGER.error(response.toString());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
+
     @ExceptionHandler(value = TypeMismatchException.class)
-    public final ResponseEntity<NumberConverterResponse> handleTypeMismatchException(){
+    public final ResponseEntity<NumberConverterResponse> handleTypeMismatchException(WebRequest webRequest){
+        ServletWebRequest request = (ServletWebRequest) webRequest;
+        LOGGER.error("Requested URI: "+request.getRequest().getRequestURI() +" - Provided input \""+getQueryParameter(webRequest) + "\" is invalid.  Input should be Integer in the range [1-3999]");
         NumberConverterError error = new NumberConverterError(ErrorDetail.INVALID_INPUT);
         NumberConverterResponse response = new NumberConverterResponse(error);
-        LOGGER.error(response.toString());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(value = Exception.class)
+    public final ResponseEntity<NumberConverterResponse> handleGenericException(Exception exception, WebRequest webRequest){
+        ServletWebRequest request = (ServletWebRequest) webRequest;
+        LOGGER.error("Requested URI: "+request.getRequest().getRequestURI() +" - input : "+getQueryParameter(webRequest)+ ", exception - " + exception.getMessage());
+        NumberConverterError error = new NumberConverterError(ErrorDetail.INTERNAL_SERVER_ERROR);
+        NumberConverterResponse response = new NumberConverterResponse(error);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    private String getQueryParameter(WebRequest request){
+
+        Iterator<String> iterator = request.getParameterNames();
+        StringBuilder sb = new StringBuilder();
+        while(iterator.hasNext()){
+            String key = iterator.next();
+            sb.append(request.getParameter(key));
+        }
+        return sb.toString();
     }
 
 }
